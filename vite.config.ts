4e0cf10cf6277c610fc081dca7262303e59f1c0b
Miyako24 <dreamgardens24@gmail.com>
2025-05-12 13:57:02 -0400

    updated vite to allow render wesbite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   envDir: './env',
   plugins: [react()],
   server: {
+    allowedHosts: ['api-candidate-search.onrender.com'], // Add your Render domain here
     host: '0.0.0.0',                   // Bind to all IPs (required by Render)
     port: Number(process.env.PORT) || 5173, // Use Render's assigned port or fallback locally
   },
